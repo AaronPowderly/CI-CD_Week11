b6a5cec64f74dd242a3596bb3500517ee63a6def
AARONPOWDERLY-STUDEN <G00434425@atu.ie>
2025-12-03 14:51:48 +0000

    Added GreetingController and added to application.properties

---
+++ src/main/java/ie/atu/cicdweek11/GreetingController.java
@@ -0,0 +1,20 @@
+package ie.atu.cicdweek11;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/health")
+    public String health(){
+        return "ok";
+    }
+
+    @GetMapping("/api/greet/{name}")
+    public String greet(@PathVariable String name){
+        return "Hello " + name + " from Service AI";
+    }
+}
