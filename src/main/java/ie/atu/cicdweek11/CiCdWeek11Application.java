bc818f5b60be48aecf4ad948ff92718c36ddbb0d
AARONPOWDERLY-STUDEN <G00434425@atu.ie>
2025-12-03 14:30:24 +0000

    Intial commit

---
+++ src/main/java/ie/atu/cicdweek11/CiCdWeek11Application.java
@@ -0,0 +1,13 @@
+package ie.atu.cicdweek11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdWeek11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdWeek11Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/cicdweek11/CiCdWeek11ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.cicdweek11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdWeek11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
